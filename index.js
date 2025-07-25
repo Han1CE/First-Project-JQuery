506215e82ae96df7333d25b647848240f88a1d95
Francis Gerald Oaquiera <francisgerald.dl.oaquiera@gmail.com>
2025-07-25 15:51:27 +0800

    commit

--- index.js
+++ index.js
@@ -3,48 +3,60 @@ const maxNoClicks = 6;
 let noScale = 1;
 let minNoScale = 0.4;
 
-const happyCatGif = "images/cute_cat_really.gif"
-const catGifs = ["images/cute_cat_asking.gif", "images/cute_cat_begging.gif", "images/cute_cat_please.gif", "images/cute_cat_areyousure.gif", "images/cute_cat_say_yes!!!.gif", "images/DiCarpio.gif" ]
-const buttonMessages = ["Are you sure??", "Pookie please!", "Pookie PLEASE! PLEASE!","Sure, sure sure???", "You can't do this to me!", "Hehehe"];
+const happyCatGif = "images/cute_cat_really.gif";
+const catGifs = [
+    "images/cute_cat_asking.gif",
+    "images/cute_cat_begging.gif",
+    "images/cute_cat_please.gif",
+    "images/cute_cat_areyousure.gif",
+    "images/cute_cat_say_yes!!!.gif",
+    "images/DiCarpio.gif"
+];
+const buttonMessages = [
+    "Are you sure??",
+    "Pookie please!",
+    "Pookie PLEASE! PLEASE!",
+    "Sure, sure sure???",
+    "You can't do this to me!",
+    "Hehehe"
+];
 
-// $(".no").on("click", function(){
-//     $("img").attr("src", catGifs[1]);
-//     $("h1").text("Please be my date")
-//     $(".no").css("width", "100px")
-// })
+function adjustYesButton() {
+    // Use relative units for responsiveness
+    $(".yes").css({
+        "width": "80vw",
+        "max-width": "300px",
+        "font-size": "2.5em"
+    });
+}
 
-// $("button").addClass("btn");
-
-// $(".yes").on("click", function(){
-//     $("h1").text("Yayy finally!! ~~~")
-//     $("img").attr("src", catGifs[4])
-//     $("button").remove()
-// })
-
-$(".no").on("click", function(){
-    if (noClicks < maxNoClicks){
-        $("img").attr("src", catGifs[noClicks])
+$(".no").on("click", function () {
+    if (noClicks < maxNoClicks) {
+        $("img").attr("src", catGifs[noClicks]);
         $("h1").text(buttonMessages[noClicks % maxNoClicks]);
     } else {
         $(".no").remove();
-        $(".yes").css("width", "300px")
-        $(".yes").css("font-size", "2.5em")
+        adjustYesButton();
     }
-    
-    
 
     if (noScale > minNoScale) {
         noScale -= 0.2;
         $(".no").css("transform", `scale(${noScale})`);
     }
 
-    noClicks++
+    noClicks++;
 });
 
+$(".yes").on("click", function () {
+    $("img").attr("src", happyCatGif);
+    $("h1").text("Really?! Yayyy finally! ~~~ !").css("color", "red");
+    $("button").remove();
+    $("h1").before('<a href="https://www.facebook.com/FrancisFgo012">Now click me and message me!</a>');
+});
 
-$(".yes").on("click", function(){
-    $("img").attr("src", happyCatGif)
-    $("h1").text("Really?! Yayyy finally! ~~~ !").css("color", "red")
-    $("button").remove()
-    $("h1").before('<a href="https://www.facebook.com/FrancisFgo012">Now click me and message me!</a>')
+// Optional: Adjust button size on window resize
+$(window).on("resize", function () {
+    if ($(".yes").length) {
+        adjustYesButton();
+    }
 });
